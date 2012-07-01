54dc3a65b1d3b9beb182b37e4124c597c38b3e95
Christophe Dubray <christophe.dubray@gmail.com>
2012-07-02 01:02:08 +0200

    made the loader a bit more complex but hopefully it will result in faster less complex procedure when developing plugins

---
+++ loader.debug.js
@@ -0,0 +1,33 @@
+﻿// Used by the plugins to get common javascripts shared by both the PartyMachine host and the plugins.
+
+(function () {
+
+	function getUrlParams() {
+		var urlParams = {};
+		(function () {
+			var e,
+				a = /\+/g,  // Regex for replacing addition symbol with a space
+				r = /([^&=]+)=?([^&]*)/g,
+				d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+				q = window.location.search.substring(1);
+
+			while (e = r.exec(q))
+				urlParams[d(e[1])] = d(e[2]);
+		})();
+
+		return urlParams;
+	};
+
+	var urlQuerystring = getUrlParams();
+	var encodedLoaderUrl = urlQuerystring["loader"];
+	var loaderUrl = decodeURIComponent(encodedLoaderUrl);
+
+	if (typeof encodedLoaderUrl === "undefined") {
+		var defaultUrl = 'http://mrcheng.github.com/Partymaskinen/partymachine.client.loader.js?pluginDev=1';
+		alert("Missing url to loader in querystring, defaulting to " + defaultUrl);
+		loaderUrl = defaultUrl;
+	}
+
+	document.write('<scr' + 'ipt type="text/javascript" src="' + loaderUrl + '"></scr' + 'ipt>');
+
+})();
